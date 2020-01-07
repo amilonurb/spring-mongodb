a0d7d50c98a572da17fdbc03db6e80ef80b5cef6
amilonurb <brunolima.18@outlook.com>
2020-01-07 00:47:06 -0300

    Tratamento de exceções personalizado

---
+++ src/main/java/br/com/brlima/springmongo/resources/RestExceptionHandler.java
@@ -0,0 +1,20 @@
+package br.com.brlima.springmongo.resources;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.brlima.springmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> handleObjectNotFound(ObjectNotFoundException exception, HttpServletRequest request) {
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase(), exception.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
